069bc490d0e28be62785b7e0fcd52a2c1e358764
trzopa <kuba.trzop30@gmail.com>
2025-11-29 15:12:33 +0100

    add new function in pipline class

--- src/train models.py
+++ src/train models.py
@@ -1,24 +1,28 @@
+from pathlib import Path
+
+import uniform
+from imblearn.over_sampling import SMOTE
+from lightgbm import LGBMClassifier
+from scipy.stats import randint, uniform
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
-from imblearn.over_sampling import SMOTE
-from importlib import import_module
-
-from sklearn.impute import SimpleImputer, KNNImputer
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold
-from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline as SklearnPipeline  # Zmieniono nazwę, żeby uniknąć konfliktu
+
+from utils import load_config, load_data
 
 
 class Pipeline:
     def __init__(self, random_state=42):
         self.random_state = random_state
+        self.DIST_MAP = {"randint": randint, "uniform": uniform}
 
     def preprocessing_data(self, data, preprocessing_config):
         mean_cols = [col for col, method in preprocessing_config.items() if method == "mean"]
@@ -33,81 +37,102 @@ class Pipeline:
             data[knn_cols] = knn_inputer.fit_transform(data[knn_cols])
         return data
 
-    def prepare_features(self, data, target_column, preprocessing_config):
-        X = data.drop(column=[target_column])
-        y = data[target_column]
-        X = self.preprocessing_data(X, preprocessing_config)
-        return X, y
+    def apply_balancing(self, X, y):
+        balanced = SMOTE(random_state=self.random_state)
+        X_balanced, y_balanced = balanced.fit_resample(X, y)
+        return X_balanced, y_balanced
 
-    def scaling(self, scalers_config, scaler_name):
-        if scaler_name == "NoScaling" or scalers_config.get(scaler_name) is None:
-            return FunctionTransformer(lambda x: x, validate=False)
+    def get_model_class(self, model_name):
+        models = {
+            "LogisticRegression": LogisticRegression,
+            "KNeighborsClassifier": KNeighborsClassifier,
+            "SVC": SVC,
+            "NaiveBayes": GaussianNB,
+            "DecisionTreeClassifier": DecisionTreeClassifier,
+            "RandomForestClassifier": RandomForestClassifier,
+            "XGBClassifier": XGBClassifier,
+            "LGBMClassifier": LGBMClassifier,
+        }
+        return models.get(model_name, None)
 
-        class_path = scalers_config[scaler_name]
-        module_name, class_name = class_path.rsplit('.', 1)
+    def get_param_distribution(self, config_name, model_name):
+        model_config = config_name[model_name]
+        param_distributions = {}
 
-        module = import_module(module_name)
-        scaler_class = getattr(module, class_name)
+        for param_name, spec in model_config.items():
+            if isinstance(spec, list):
+                param_distributions[param_name] = spec
 
-        return scaler_class()
+            elif isinstance(spec, dict) and "distribution" in spec:
+                dist_name = spec["distribution"]
+                dist_cls = self.DIST_MAP[dist_name]
 
-    def parse_models_config(self, models_config):
-        models = []
+                if dist_name == "randint":
+                    param_distributions[param_name] = dist_cls(
+                        low=spec["low"], high=spec["high"]
+                    )
+                elif dist_name == "uniform":
+                    param_distributions[param_name] = dist_cls(
+                        loc=spec["loc"], scale=spec["scale"]
+                    )
 
-        for model_name, params in models_config.items():
-            fixed_params = {}
-            search_space = {}
+        return param_distributions
 
-            for param_name, param_value in params.items():
-                if isinstance(param_value, dict) and 'distribution' in param_value:
-                    search_space[param_name] = param_value
-                elif isinstance(param_value, list):
-                    search_space[param_name] = param_value
-                else:
-                    fixed_params[param_name] = param_value
-
-            models.append({
-                'model_name': model_name,
-                'fixed_params': fixed_params,
-                'search_space': search_space
-            })
-
-        return models
-
-    def grid_search_cv(self, pipe, random_state):
+    def grid_search_cv(self, X, y, pipe, param_dist):
         search = RandomizedSearchCV(
-            estimator=pipe,
+            pipe,
+            param_distributions=param_dist,
+            n_iter=5,
+            cv=5,
+            scoring='f1',
+            random_state=self.random_state,
             n_jobs=-1,
-            scoring="f1",
-            verbose=1)
+            verbose=1
+        )
+        search.fit(X, y)
+        return search.best_params_
 
-        return search
+    def create_pipeline_with_scaler(self, model_cls, scaler=None):
+        steps = []
+        if scaler:
+            steps.append(("scaler", scaler))
+        steps.append(("clf", model_cls(random_state=self.random_state)))
+        return SklearnPipeline(steps)
 
-    def run(self, data, target_column, preprocessing_config):
-        X = data.drop[column=target_column]
-        y = data[target_column]
-        X = self.preprocessing_data(X, preprocessing_config)
-        scaler1 = StandardScaler()
-        scaler2 = MinMaxScaler()
-        X_stan, y_stan = scaler1.fit_transform(X, y)
+    def run_pipline(self, data, preprocessing_file, model_file, model_name):
+        data_processed = self.preprocessing_data(data, preprocessing_file)
+        X_processed = data_processed.drop(columns=["target"])
+        y = data_processed["target"]
+        X_balanced, y_balanced = self.apply_balancing(X_processed, y)
 
-        X_mm, y_mm = scaler2.fit_transform(X, y)
-        balance = SMOTE()
-        X_b, y_b = balance.fit_resample(X, y)
-        logic_cl = LogisticRegression()
-        logic_cl.fit(X_b, y_b)
-        treeml = DecisionTreeClassifier()
-        treeml.fit(X_b, y_b)
-        forestml = RandomForestClassifier()
-        forestml.fit(X_b, y_b)
-        xbml = XGBClassifier()
-        xbml.fit(X_b, y_b)
-        lxbml = LGBMClassifier()
-        lxbml.fit(X_b, y_b)
-        gml = GaussianNB()
-        gml.fit(X_b, y_b)
-        svcml = SVC()
-        svcml.fit(X_b, y_b)
-        knml = KNeighborsClassifier()
-        knml.fit(X_b, y_b)
-        StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        param_dist = self.get_param_distribution(model_file, model_name)
+        model_cls = self.get_model_class(model_name)
+        results = {}
+        pipelines_to_test = {
+            "standard": self.create_pipeline_with_scaler(model_cls, scaler=StandardScaler()),
+            "minmax": self.create_pipeline_with_scaler(model_cls, scaler=MinMaxScaler()),
+            "none": self.create_pipeline_with_scaler(model_cls, scaler=None),
+        }
+
+        for scaler_name, pipe_with_scaler in pipelines_to_test.items():
+            print(f"Trening i tuning z {scaler_name} scalerem...")
+            best_model = self.grid_search_cv(X_balanced, y_balanced, pipe_with_scaler, param_dist)
+            results[scaler_name] = best_model
+
+        return results
+
+
+# poprawić pipline
+# wywolac dla kazdego modelu
+# zapisac wyniki
+#
+
+if __name__ == "__main__":
+    root = Path.cwd().parent
+    model_file = load_config("config/models.yaml")
+    preprocessing_file = load_config("config/preprocessing.yaml")
+    data = load_data("/Users/trzopa/Benchmark-modeli-klasyfikacyjnych-na-danych-syntetycznych/data/train.csv")
+    p = Pipeline()
+    pdata = p.preprocessing_data(data, preprocessing_file)
+    LogisticRegression = p.run_pipline(data, preprocessing_file, model_file, "LogisticRegression")
+    print(LogisticRegression)
