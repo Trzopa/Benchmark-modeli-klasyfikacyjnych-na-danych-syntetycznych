45badeedf0d99017a7d8b5e26be3a4b614d56a4e
trzopa <kuba.trzop30@gmail.com>
2025-12-18 17:03:06 +0100

    18.12

--- src/train models.py
+++ src/train models.py
@@ -3,7 +3,7 @@ from utils import load_config, load_data, to_dataframe
 from pipeline import Pipeline
 
 root = Path.cwd().parent
-model_file = load_config("config/models.yaml")
+model_file = load_config("config/model_params.yaml")
 preprocessing_file = load_config("config/preprocessing.yaml")
 data = load_data(f"{root}/data/train.csv")
 p = Pipeline()
