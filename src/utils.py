67ea1ccbf30b708f3a3d785a643ad1d375f04362
trzopa <kuba.trzop30@gmail.com>
2025-12-26 16:17:22 +0100

    26.12

--- src/utils.py
+++ src/utils.py
@@ -5,59 +5,53 @@ from pathlib import Path
 import joblib
 import pandas as pd
 import yaml
+import json
 
 
 def load_data(path):
-    with open(path, "r") as f:
-        file = pd.read_csv(f)
-    return file
+    return pd.read_csv(path)
 
 
 def load_config(path):
     with open(path, "r") as f:
-        file = yaml.safe_load(f)
-    return file
+        return yaml.safe_load(f)
 
 
 def save_model(model, model_path):
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
-
     joblib.dump(model, model_path)
     print(f"💾 Model saved: {model_path}")
     return model_path
 
 
-def save_params_model_with_best_params(model, scaler, balancing_name, training_time, accuracy_score_val,
-                                       precision_score_val,
-                                       recall_score_val,
-                                       f1_score_val, roc_auc_score_val, best_params=None):
+def save_params_model_with_best_params(
+        model, scaler, balancing_name, training_time, accuracy_score_val,
+        precision_score_val, recall_score_val, f1_score_val, roc_auc_score_val,
+        best_params=None, model_path=None
+):
     result = {
         "model": model,
         "scaler": scaler,
-        "balancing name": balancing_name,
-        "time_training": training_time,
+        "balancing_name": balancing_name,
+        "training_time": training_time,
         "accuracy_score": accuracy_score_val,
         "precision_score": precision_score_val,
         "recall_score": recall_score_val,
         "f1_score": f1_score_val,
         "roc_auc_score": roc_auc_score_val,
-        "best_params": best_params,
-
+        "best_params": json.dumps(best_params) if best_params else "{}",  # ⭐ dict → string
+        "model_path": model_path
     }
     return result
 
 
 def to_dataframe(results_list, name_folder):
     df = pd.DataFrame(results_list)
-
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
     root = Path.cwd().parent
     results_dir = root / "results" / name_folder
     results_dir.mkdir(parents=True, exist_ok=True)
-
     file_path = results_dir / f"results_{timestamp}.csv"
     df.to_csv(file_path, index=False)
     print(f"Wyniki zapisane do: {file_path}")
-
     return df
