cc8215978d45efc46273295e0149a6ab4a6acb8d
trzopa <kuba.trzop30@gmail.com>
2026-01-03 17:51:10 +0100

    03.01

--- src/train models.py
+++ src/train models.py
@@ -8,6 +8,5 @@ preprocessing_file = load_config("config/preprocessing.yaml")
 data = load_data(f"{root}/data/train.csv")
 models_dir = f"{root}/results/models"
 p = Pipeline(models_dir=models_dir)
-# pp = p.run_pipeline(data, "LGBMClassifier",model_file, preprocessing_file )
 all_models = p.run_all_models(data, model_file, preprocessing_file)
 to_dataframe(all_models, "metrics")
